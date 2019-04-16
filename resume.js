a5e0f3025e03441d5156be5e9b5655c62a091590
Gavin Fielder <gfielder@e1z2r7p23.42.us.org>
2019-04-15 23:55:55 -0700

    redesigned layout

--- resume.js
+++ resume.js
@@ -6,21 +6,36 @@
 //   By: gfielder <marvin@42.fr>                    +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2019/04/14 14:56:01 by gfielder          #+#    #+#             //
-//   Updated: 2019/04/14 21:57:32 by gfielder         ###   ########.fr       //
+//   Updated: 2019/04/15 23:02:39 by gfielder         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 //Requires jquery
 
+var include_educational;
+var current_filter;
+var custom_filter;
 
 $(document).ready(function()
 {
+	include_educational = true;
+	current_filter = 'All Projects';
+	custom_filter = false;
+	$('#educational-tagsel-checkbox').change(function() {
+		include_educational = this.checked;
+		if (current_filter == 'All Projects')
+			ShowAllProjects();
+		else
+			ShowProjectsWithTag(current_filter);
+		UpdateCurrentFilterText();
+	});
 	//Set up event listeners
-	$('.project').click(ExpandableClick);
 	$('.expandable-section-header').click(ExpandableClick);
 	$('.expandable-section-clicktoexpand').click(ExpandableClick);
 	$('.tagsel').not('#tagsel-all').click(OnTagSelect);
 	$('#tagsel-all').click(ShowAllProjects);
+	$('.project-manual-hide').click(OnProjectHideClick);
+	$('.job-entry-manual-hide').click(OnJobEntryHideClick);
 	//Open the Projects Section by default.
 	var proj = $('#projects-section');
 	proj.children("#hidden-inline").html("Click to collapse...");
@@ -32,7 +47,6 @@ function ExpandableClick(eventObject)
 	var expanded = $(eventObject.target).children("#expanded");
 	var hidden = $(eventObject.target).children("#hidden");
 	var hiddeninline = $(eventObject.target).children("#hidden-inline");
-	var tags = $(eventObject.target).children("#project-tags");
 
 	if (expanded.length == 0)
 	{
@@ -40,7 +54,6 @@ function ExpandableClick(eventObject)
 		expanded = $(eventObject.target.parentElement).children("#expanded");
 		hidden = $(eventObject.target.parentElement).children("#hidden");
 		hiddeninline = $(eventObject.target.parentElement).children("#hidden-inline");
-		tags = $(eventObject.target.parentElement).children("#project-tags");
 	}
 	if (expanded.css('display') == "none")
 	{
@@ -48,7 +61,6 @@ function ExpandableClick(eventObject)
 		expanded.css('display', 'block');
 		hidden.css('display', 'none');
 		hiddeninline.html("Click to collapse...");
-		tags.css('display', 'block');
 	}
 	else
 	{
@@ -56,16 +68,38 @@ function ExpandableClick(eventObject)
 		expanded.css('display', 'none');
 		hidden.css('display', 'block');
 		hiddeninline.html("Click to expand...");
-		tags.css('display', 'none');
 	}
-}	
+}
 
 function ShowAllProjects()
 {
+	current_filter = 'All Projects';
 	var projs = $(".project");
+	var tags;
 	projs.each(function(index, element) {
-		$(element).css('display', 'block');
+		tags=$(element).children("#project-tags").html();
+		if (tags.includes('Educational') && include_educational == false)
+			$(element).css('display', 'none');
+		else
+			$(element).css('display', 'block');
 	});
+	custom_filter = false;
+	UpdateCurrentFilterText();
+}
+
+function UpdateCurrentFilterText()
+{
+	if (custom_filter)
+	{
+		$('#current-filter').html('*Custom*');
+	}
+	else
+	{
+		if (include_educational)
+			$('#current-filter').html(current_filter + ', Include Educational');
+		else
+			$('#current-filter').html(current_filter);
+	}
 }
 
 function OnTagSelect(eventObject)
@@ -75,13 +109,40 @@ function OnTagSelect(eventObject)
 
 function ShowProjectsWithTag(tag)
 {
+	current_filter = tag;
 	var projs = $(".project");
 	var tags;
 	projs.each(function(index, element) {
 		tags=$(element).children("#project-tags").html();
 		if (tags.includes(tag))
-			$(element).css('display', 'block');
+		{
+			if (tags.includes('Educational') && include_educational == false)
+				$(element).css('display', 'none');
+			else
+				$(element).css('display', 'block');
+		}
 		else
 			$(element).css('display', 'none');
 	});
+	custom_filter = false;
+	UpdateCurrentFilterText();
 }
+
+function OnProjectHideClick(eventObject)
+{
+	proj = $(eventObject.target.parentElement.parentElement);
+	proj.css('display', 'none');
+	custom_filter = true;
+	UpdateCurrentFilterText();
+}
+
+function OnJobEntryHideClick(eventObject)
+{
+	job = $(eventObject.target.parentElement.parentElement);
+	job.css('display', 'none');
+	$('#job-filter-text').css('display', 'block');
+}
+
+
+
+
